972c909d93094ae292976ef10a2fd3b5e4569e95
Amanda Cooksey <amanda.cooksey@atlas-login-2.hpc.msstate.edu>
2024-11-13 11:57:16 -0600

    only outputs lines with annotations

--- kofam_to_api_pipeline/merge_data.py
+++ kofam_to_api_pipeline/merge_data.py
@@ -34,12 +34,12 @@ if kofam == "no" and species != "NA":
     spec_pathway.columns = [species, f"{species}pathway"]
     list_pathway_spec.columns = [f"{species}pathway", f"{species}pathname"]
 #MERGE DATAFRAMES INTO ONE FOR REFERENCE PATHWAYS
-    ncbi_spec_ko = pd.merge(ncbi_spec, spec_ko, on=species, how='left')
-    ncbi_spec_ko_pathway = pd.merge(ncbi_spec_ko, ko_pathway, on='ko', how='left')
+    ncbi_spec_ko = pd.merge(ncbi_spec, spec_ko, on=species, how='inner')
+    ncbi_spec_ko_pathway = pd.merge(ncbi_spec_ko, ko_pathway, on='ko', how='inner')
     ncbi_spec_ko_pathway_pathname = pd.merge(ncbi_spec_ko_pathway, pathway, on='pathway', how='left')
     ncbi_spec_ko_pathway_pathname.to_csv(f"{species}_direct_KEGG_ref.tsv", sep='\t', index=False)
 #MERGE DATAFRAMES INTO ONE FOR { species } PATHWAYS
-    ncbi_spec_ko_specpath = pd.merge(ncbi_spec_ko, spec_pathway, on=species, how='left')
+    ncbi_spec_ko_specpath = pd.merge(ncbi_spec_ko, spec_pathway, on=species, how='inner')
     ncbi_spec_ko_specpath_specpathname = pd.merge(ncbi_spec_ko_specpath, list_pathway_spec, on=f"{species}pathway", how='left')
     ncbi_spec_ko_specpath_specpathname.to_csv(f"{species}_direct_KEGG_{species}.tsv", sep='\t', index=False)
 elif kofam == "yes" and species == "NA":
@@ -52,7 +52,7 @@ elif kofam == "yes" and species == "NA":
     ko_pathway.columns = ['pathway', 'ko']
     pathway.columns = ['pathway', 'pathname']
 #MERGE DATAFRAMES INTO ONE FOR REFERENCE PATHWAYS
-    ncbi_ko_pathway = pd.merge(ncbi_ko, ko_pathway, on='ko', how='left')
+    ncbi_ko_pathway = pd.merge(ncbi_ko, ko_pathway, on='ko', how='inner')
     ncbi_ko_pathway_pathname = pd.merge(ncbi_ko_pathway, pathway, on='pathway', how='left')
     ncbi_ko_pathway_pathname.to_csv(f"direct_KEGG_ref.tsv", sep='\t', index=False)
 elif kofam == "yes" and species != "NA":
@@ -71,12 +71,12 @@ elif kofam == "yes" and species != "NA":
     spec_pathway.columns = ['species', f"{species}pathway"]
     list_pathway_spec.columns = [f"{species}pathway", f"{species}pathname"]
 #MERGE DATAFRAMES INTO ONE FOR REFERENCE PATHWAYS
-    ncbi_spec_ko = pd.merge(ncbi_ko, spec_ko, on='ko', how='left')
-    ncbi_spec_ko_pathway = pd.merge(ncbi_spec_ko, ko_pathway, on='ko', how='left')
+    ncbi_spec_ko = pd.merge(ncbi_ko, spec_ko, on='ko', how='inner')
+    ncbi_spec_ko_pathway = pd.merge(ncbi_spec_ko, ko_pathway, on='ko', how='inner')
     ncbi_spec_ko_pathway_pathname = pd.merge(ncbi_spec_ko_pathway, pathway, on='pathway', how='left')
     ncbi_spec_ko_pathway_pathname.to_csv(f"{species}_direct_KEGG_ref.tsv", sep='\t', index=False)
 #MERGE DATAFRAMES INTO ONE FOR { species } PATHWAYS
-    ncbi_spec_ko_specpath = pd.merge(ncbi_spec_ko, spec_pathway, on='species', how='left')
+    ncbi_spec_ko_specpath = pd.merge(ncbi_spec_ko, spec_pathway, on='species', how='inner')
     ncbi_spec_ko_specpath_specpathname = pd.merge(ncbi_spec_ko_specpath, list_pathway_spec, on=f"{species}pathway", how='left')
     ncbi_spec_ko_specpath_specpathname.to_csv(f"{species}_direct_KEGG_{species}.tsv", sep='\t', index=False)
 else:
