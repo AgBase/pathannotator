2e2f3ecd0300af59b38db67fcbf02714dff80d1d
Amanda Cooksey <amandamcooksey@gmail.com>
2024-12-06 16:38:54 -0700

    kofam_masterscript.sh

--- kofam_to_api_pipeline/kofam_masterscript.sh
+++ kofam_to_api_pipeline/kofam_masterscript.sh
@@ -111,20 +111,20 @@ else #ELSE MEANS THESE ARE NOT NCBI PROTEIN IDS.
 	fi
 fi
 
-if [ -f "$3"/link_ko_pathway.tsv ]; then rm "$3"/link_ko_pathway.tsv; fi
-if [ -f "$3"/list_pathway.tsv ]; then rm "$3"/list_pathway.tsv; fi
-if [ -f "$3"/conv_ncbi-proteinid_"$1".tsv ]; then rm "$3"/conv_ncbi-proteinid_"$1".tsv; fi
-if [ -f "$3"/link_"$1"_ko.tsv ]; then rm "$3"/link_"$1"_ko.tsv; fi
-if [ -f "$3"/link_pathway_"$1".tsv ]; then rm "$3"/link_pathway_"$1".tsv; fi
-if [ -f "$3"/list_pathway_"$1".tsv ]; then rm "$3"/list_pathway_"$1".tsv; fi
-if [ -f "$3"/deflines.tmp ]; then rm "$3"/deflines.tmp; fi
-if [ -f "$3"/ko_ncbi.tsv ]; then rm "$3"/ko_ncbi.tsv; fi
-if [ -f "$3"/Fbgn_CG.tsv ]; then rm "$3"/Fbgn_CG.tsv; fi
-if [ -f "$3"/Fbgn_groupid.tsv ]; then rm "$3"/Fbgn_groupid.tsv; fi
-if [ -f "$3"/pathway_group_data_fb_2024_05.tsv ]; then 	rm "$3"/pathway_group_data_fb_2024_05.tsv; fi
-if [ -f "$3"/pathway_group_data_fb_2024_05.tsv.gz ]; then rm "$3"/pathway_group_data_fb_2024_05.tsv.gz; fi
-if [ -f "$3"/fbgn_annotation_ID_fb_2024_05.tsv ]; then 	rm "$3"/fbgn_annotation_ID_fb_2024_05.tsv; fi
-if [ -f "$3"/fbgn_annotation_ID_fb_2024_05.tsv.gz ]; then  rm "$3"/fbgn_annotation_ID_fb_2024_05.tsv.gz; fi
+#if [ -f "$3"/link_ko_pathway.tsv ]; then rm "$3"/link_ko_pathway.tsv; fi
+#if [ -f "$3"/list_pathway.tsv ]; then rm "$3"/list_pathway.tsv; fi
+#if [ -f "$3"/conv_ncbi-proteinid_"$1".tsv ]; then rm "$3"/conv_ncbi-proteinid_"$1".tsv; fi
+#if [ -f "$3"/link_"$1"_ko.tsv ]; then rm "$3"/link_"$1"_ko.tsv; fi
+#if [ -f "$3"/link_pathway_"$1".tsv ]; then rm "$3"/link_pathway_"$1".tsv; fi
+#if [ -f "$3"/list_pathway_"$1".tsv ]; then rm "$3"/list_pathway_"$1".tsv; fi
+#if [ -f "$3"/deflines.tmp ]; then rm "$3"/deflines.tmp; fi
+#if [ -f "$3"/ko_ncbi.tsv ]; then rm "$3"/ko_ncbi.tsv; fi
+#if [ -f "$3"/Fbgn_CG.tsv ]; then rm "$3"/Fbgn_CG.tsv; fi
+#if [ -f "$3"/Fbgn_groupid.tsv ]; then rm "$3"/Fbgn_groupid.tsv; fi
+#if [ -f "$3"/pathway_group_data_fb_2024_05.tsv ]; then 	rm "$3"/pathway_group_data_fb_2024_05.tsv; fi
+#if [ -f "$3"/pathway_group_data_fb_2024_05.tsv.gz ]; then rm "$3"/pathway_group_data_fb_2024_05.tsv.gz; fi
+#if [ -f "$3"/fbgn_annotation_ID_fb_2024_05.tsv ]; then 	rm "$3"/fbgn_annotation_ID_fb_2024_05.tsv; fi
+#if [ -f "$3"/fbgn_annotation_ID_fb_2024_05.tsv.gz ]; then  rm "$3"/fbgn_annotation_ID_fb_2024_05.tsv.gz; fi
 #if [ -f "$3"/dmel-all-translation-*.fasta]; then  rm "$3"/dmel-all-translation-*.fasta; fi
 #if [ -f "$3"/dmel-all-translation-*.fasta.gz ]; then  rm "$3"/dmel-all-translation-*.fasta.gz; fi
 #if [ -f "$3"/phmmacc.txt ]; then  rm "$3"/phmmacc.txt; fi
