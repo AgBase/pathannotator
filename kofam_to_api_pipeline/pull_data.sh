6e246a23a48a7e25a247b9675ac9ebdf487c1a31
Amanda Cooksey <amandamcooksey@gmail.com>
2024-12-12 13:41:32 -0700

    x

--- kofam_to_api_pipeline/pull_data.sh
+++ kofam_to_api_pipeline/pull_data.sh
@@ -76,12 +76,14 @@ if [ "$1" == "dme" ];
 then
 	echo "Species code is 'dme'; pulling Flybase data now."
 	#THIS PULLS THE FLYBASE ANNOTATIONS
-	wget -r -nd -A gz --accept-regex "signaling_pathway_group_data*" -O $3/signaling_pathway_group_data_latest.tsv.gz 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
-	wget -r -nd -A gz --accept-regex "metabolic_pathway_group_data*" -O $3/metabolic_pathway_group_data_latest.tsv.gz 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
-	gunzip $3/signaling_pathway_group_data_latest.tsv.gz
-	gunzip $3/metabolic_pathway_group_data_latest.tsv.gz
-	grep -v ^\## $3/signaling_pathway_group_data_latest.tsv > $3/pathway_group_data_latest.tsv
-	grep -v ^\## $3/metabolic_pathway_group_data_fb_2024_05.tsv >> $3/pathway_group_data_latest.tsv
+#	wget -r -nd -A gz --accept-regex "signaling_pathway_group_data*" -O $3/signaling_pathway_group_data_latest.tsv.gz 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
+#	wget -r -nd -A gz --accept-regex "metabolic_pathway_group_data*" -O $3/metabolic_pathway_group_data_latest.tsv.gz 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	wget -r -nd -A gz --accept-regex "signaling_pathway_group_data*" -P $3/ 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	wget -r -nd -A gz --accept-regex "metabolic_pathway_group_data*" -P $3/ 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	gunzip $3/signaling_pathway_group_data_*
+	gunzip $3/metabolic_pathway_group_data_*
+	grep -v ^\## $3/signaling_pathway_group_data_* > $3/pathway_group_data_latest.tsv
+	grep -v ^\## $3/metabolic_pathway_group_data_* >> $3/pathway_group_data_latest.tsv
 	cut -f 1,3,6 $3/pathway_group_data_latest.tsv > $3/Fbgn_groupid.tsv
 
 	wget -r -nd -A gz --accept-regex "fbgn_annotation_ID_*" -O $3/fbgn_annotation_ID_latest.tsv.gz 'ftp://ftp.flybase.org/releases/current/precomputed_files/genes/'
@@ -90,11 +92,13 @@ then
 	sed -i 's/Dmel_//g' $3/Fbgn_CG.tsv
 
 	#PULL FB PROTEIN FASTA
-	wget -r -nd -A gz --accept-regex "dmel-all-translation*" -O $3/dmel-all-translation-latest.fasta.gz 'ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/current/fasta/'
-        gunzip $3/dmel-all-translation-latest.fasta.gz
+#	wget -r -nd -A gz --accept-regex "dmel-all-translation*" -O $3/dmel-all-translation-latest.fasta.gz 'ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/current/fasta/'
+	wget -r -nd -A gz --accept-regex "dmel-all-translation*" -P $3/ 'ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/current/fasta/'
+        gunzip $3/dmel-all-translation-*
 
 	#PULL FBGN TO FBPP FILES
-	wget -r -nd -A gz  --accept-regex "fbgn_fbtr_fbpp_fb*" -O $3/fbgn_fbtr_fbpp_fb_latest.tsv.gz 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
-	gunzip $3/fbgn_fbtr_fbpp_fb_latest.tsv.gz
-	grep -v ^\## $3/fbgn_fbtr_fbpp_fb_latest.tsv | cut -f 1,3 > $3/Fbgn_fbpp.tsv
+#	wget -r -nd -A gz  --accept-regex "fbgn_fbtr_fbpp_fb*" -O $3/fbgn_fbtr_fbpp_fb_latest.tsv.gz 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	wget -r -nd -A gz  --accept-regex "fbgn_fbtr_fbpp_fb*" -P $3/ 'ftp://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	gunzip $3/fbgn_fbtr_fbpp_fb_*
+	grep -v ^\## $3/fbgn_fbtr_fbpp_fb_* | cut -f 1,3 > $3/Fbgn_fbpp.tsv
 fi
