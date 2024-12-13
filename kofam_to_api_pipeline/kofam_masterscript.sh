a6861ee9cda19a3338d1d97012dcd297fffcab34
Amanda Cooksey <amandamcooksey@gmail.com>
2024-12-13 11:57:41 -0700

    x

--- kofam_to_api_pipeline/kofam_masterscript.sh
+++ kofam_to_api_pipeline/kofam_masterscript.sh
@@ -136,7 +136,7 @@ if [ -f "$3"/deflines.tmp ]; then rm "$3"/deflines.tmp; fi
 if [ -f "$3"/ko_ncbi.tsv ]; then rm "$3"/ko_ncbi.tsv; fi
 if [ -f "$3"/Fbgn_CG.tsv ]; then rm "$3"/Fbgn_CG.tsv; fi
 if [ -f "$3"/Fbgn_groupid.tsv ]; then rm "$3"/Fbgn_groupid.tsv; fi
-if [ -f "$3"/pathway_group_data_lastest.tsv ]; then rm $3/pathway_group_data_latest.tsv; fi
+if [ -f "$3"/pathway_group_data_latest.tsv ]; then rm $3/pathway_group_data_latest.tsv; fi
 if [ -n "$(ls $3/*pathway_group_data_fb* 2>/dev/null)" ]; then rm $3/*pathway_group_data_fb*; fi
 if [ -n "$(ls $3/fbgn_annotation_ID_fb* 2>/dev/null)" ]; then rm $3/fbgn_annotation_ID_fb*; fi
 if [ -n "$(ls $3/dmel-all-translation*.fasta* 2>/dev/null)" ]; then rm $3/dmel-all-translation*.fasta*; fi
