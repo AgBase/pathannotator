5fd7512ae4693f30a977ce22d9b0c14214b376c2
Amanda Cooksey <amandamcooksey@gmail.com>
2025-01-21 11:10:36 -0700

    semicolon

--- kofam_to_api_pipeline/pathannotator.sh
+++ kofam_to_api_pipeline/pathannotator.sh
@@ -279,4 +279,4 @@ if [ -f "$3"/Fbgn_fbpp.tsv ]; then rm "$3"/Fbgn_fbpp.tsv; fi
 if [ -f "$3"/FB_phmmer.txt ]; then rm "$3"/FB_phmmer.txt; fi
 if [ -d "$3"/tmp ]; then rm -r "$3"/tmp; fi
 if [ -d "$3"/tmp.txt ]; then rm  "$3"/tmp.txt; fi
-if [ -d "$3"/kofam_filtered_asterisk.txt ] then rm "$3"/kofam_filtered_asterisk.txt; fi
+if [ -d "$3"/kofam_filtered_asterisk.txt ]; then rm "$3"/kofam_filtered_asterisk.txt; fi
