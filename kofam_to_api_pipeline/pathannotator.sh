1589fd8e53ab428acd9a86b0196d67654500cf30
Amanda Cooksey <amandamcooksey@gmail.com>
2025-02-06 15:02:25 -0700

    removing temp files

--- kofam_to_api_pipeline/pathannotator.sh
+++ kofam_to_api_pipeline/pathannotator.sh
@@ -277,8 +277,10 @@ if [ -n "$(ls $3/fbgn_fbtr_fbpp_fb* 2>/dev/null)" ]; then rm $3/fbgn_fbtr_fbpp_f
 if [ -f "$3"/Fbgn_fbpp.tsv ]; then rm "$3"/Fbgn_fbpp.tsv; fi
 if [ -d "$3"/tmp ]; then rm -r "$3"/tmp; fi
 if [ -d "$3"/tmp.txt ]; then rm  "$3"/tmp.txt; fi
-#if [ -d "$3"/FB_diamond.tsv ]; then rm  "$3"/FB_diamond.tsv; fi
-#if [ -d "$3"/dia_matches.tsv ]; then rm  "$3"/dia_matches.tsv; fi
+if [ -d "$3"/FB_diamond.tsv ]; then rm  "$3"/FB_diamond.tsv; fi
+if [ -d "$3"/dia_matches.tsv ]; then rm  "$3"/dia_matches.tsv; fi
+if [ -d "$3"/diamond_out.tsv ]; then rm  "$3"/diamond_out.tsv; fi
+if [ -d "$3"/dmel_db.dmnd ]; then rm  "$3"/dmel_db.dmnd; fi
 
 endtime=$(date +%s)
 seconds=$(($endtime - $starttime))
