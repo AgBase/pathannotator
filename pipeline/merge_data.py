02604fdabb94a6281e12dc6951d91f80bf64c862
Amanda Cooksey <amandamcooksey@gmail.com>
2025-02-18 09:36:58 -0700

    tidying

---
+++ pipeline/merge_data.py
@@ -0,0 +1,176 @@
+
+#! /usr/bin/env/python
+
+import pandas as pd
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('species')
+parser.add_argument('kofam')
+parser.add_argument('indir')
+parser.add_argument('outdir')
+parser.add_argument('flybase')
+args = parser.parse_args()
+species = args.species # kegg speicies code, NA or related species if species not in KEGG
+kofam = args.kofam # yes or no
+indir = args.indir # directory with outputs from pull_data.sh
+outdir = args.outdir # directory where outputs this will go
+flybase = args.flybase #FB for Flybase annotations, NA for none
+
+#READ API TABLES INTO PANDAS DATAFRAMES
+if kofam == "no" and species != "NA":
+    ncbi_spec = pd.read_table(f"{indir}/conv_ncbi-proteinid_{species}.tsv", dtype=str)
+    spec_ko = pd.read_table(f"{indir}/link_{species}_ko.tsv", dtype=str)
+    spec_pathway = pd.read_table(f"{indir}/link_pathway_{species}.tsv", dtype=str)
+    list_pathway_spec = pd.read_table(f"{indir}/list_pathway_{species}.tsv", dtype=str)
+    ko_pathway = pd.read_table(f"{indir}/link_ko_pathway.tsv", dtype=str)
+    pathway = pd.read_table(f"{indir}/list_pathway.tsv", dtype=str)
+#ADD HEADERS TO DATAFRAME COLUMNS
+    ncbi_spec.columns = ['KEGG_genes_ID', 'Input_protein_ID']
+    spec_ko.columns = ['KEGG_KO', 'KEGG_genes_ID']
+    ko_pathway.columns = ['KEGG_ref_pathway', 'KEGG_KO']
+    pathway.columns = ['KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+    spec_pathway.columns = ['KEGG_genes_ID', f"KEGG_{species}_pathway"]
+    list_pathway_spec.columns = [f"KEGG_{species}_pathway", f"KEGG_{species}_pathway_name"]
+#MERGE DATAFRAMES INTO ONE FOR REFERENCE PATHWAYS
+    ncbi_spec_ko = pd.merge(ncbi_spec, spec_ko, on='KEGG_genes_ID', how='inner')
+    ncbi_spec_ko_pathway = pd.merge(ncbi_spec_ko, ko_pathway, on='KEGG_KO', how='inner')
+    ncbi_spec_ko_pathway_pathname = pd.merge(ncbi_spec_ko_pathway, pathway, on='KEGG_ref_pathway', how='left')
+    ncbi_spec_ko_pathway_pathname.to_csv(f"{outdir}/{species}_KEGG_ref.tsv", sep='\t', index=False)
+#MERGE DATAFRAMES INTO ONE FOR { species } PATHWAYS
+    ncbi_spec_ko_specpath = pd.merge(ncbi_spec_ko, spec_pathway, on='KEGG_genes_ID', how='inner')
+    ncbi_spec_ko_specpath_specpathname = pd.merge(ncbi_spec_ko_specpath, list_pathway_spec, on=f"KEGG_{species}_pathway", how='left')
+    ncbi_spec_ko_specpath_specpathname.to_csv(f"{outdir}/{species}_KEGG_species.tsv", sep='\t', index=False)
+    #ADD FLYBASE ANNOTATIONS WHEN DME IS THE SPECIFIED SPECIES
+    if flybase == "FB" and species == "dme":
+        #READ INTO DATAFRAMES
+        fbgn_CG = pd.read_table(f"{indir}/Fbgn_CG.tsv", dtype=str)
+        fbgn_path = pd.read_table(f"{indir}/Fbgn_groupid.tsv", dtype=str)
+        #ADD HEADERS
+        fbgn_CG.columns = ['Flybase_gene', 'KEGG_genes_ID']
+        fbgn_path.columns = ['Flybase_pathway_ID', 'Flybase_pathway_name', 'Flybase_gene']
+        #MERGE AND OUTPUT TO FILE
+        fbgn_CG_path = pd.merge(fbgn_CG, fbgn_path, on='Flybase_gene', how='inner')
+        ncbi_spec_ko['KEGG_genes_ID'] = ncbi_spec_ko['KEGG_genes_ID'].str.replace('Dmel_', '')
+        fbgn_CG_path_ncbi_spec_ko = pd.merge(ncbi_spec_ko, fbgn_CG_path, on='KEGG_genes_ID', how='inner')
+        fbgn_CG_path_ncbi_spec_ko.drop('Flybase_gene', axis=1, inplace=True)
+        fbgn_CG_path_ncbi_spec_ko = fbgn_CG_path_ncbi_spec_ko[["KEGG_genes_ID","Input_protein_ID","KEGG_KO","Flybase_pathway_ID","Flybase_pathway_name"]]
+        fbgn_CG_path_ncbi_spec_ko.to_csv(f"{outdir}/{species}_flybase.tsv", sep='\t', index=False)
+    elif flybase =="FB" and species != "dme":
+        #READ INTO DATAFRAMES
+        fbgn_CG = pd.read_table(f"{indir}/Fbgn_CG.tsv", dtype=str)
+        fbgn_path = pd.read_table(f"{indir}/Fbgn_groupid.tsv", dtype=str)
+        fbgn_dia = pd.read_table(f"{indir}/FB_diamond.tsv", dtype=str)
+        fbgn_fbpp = pd.read_table(f"{indir}/Fbgn_fbpp.tsv", dtype=str)
+        #ADD HEADERS
+        fbgn_path.columns = ['Flybase_pathway_ID', 'Flybase_pathway_name', 'Flybase_gene']
+        fbgn_CG.columns = ['Flybase_gene', 'KEGG_genes_ID']
+        fbgn_dia.columns = ['Input_protein_ID', 'Flybase_protein_ID']
+        fbgn_fbpp.columns = ['Flybase_gene', 'Flybase_protein_ID']
+        #MERGE AND OUTPUT TO FILE
+        fbgn_fbpp_dia = pd.merge(fbgn_fbpp, fbgn_dia, on='Flybase_protein_ID', how='inner')
+        fbgn_fbpp_dia_path = pd.merge(fbgn_fbpp_dia, fbgn_path, on='Flybase_gene', how='inner')
+        fbgn_fbpp_dia_path_CG = pd.merge(fbgn_fbpp_dia_path, fbgn_CG, on='Flybase_gene', how='inner')
+        fbgn_fbpp_dia_path_CG.drop('Flybase_gene', axis=1, inplace=True)
+        fbgn_fbpp_dia_path_CG = fbgn_fbpp_dia_path_CG[["KEGG_genes_ID","Input_protein_ID","Flybase_protein_ID","Flybase_pathway_ID","Flybase_pathway_name"]]
+        fbgn_fbpp_dia_path_CG.to_csv(f"{outdir}/Diamond_flybase.tsv", sep='\t', index=False)
+    else:
+        print("You have not requested Flybase annotations.")
+elif kofam == "yes" and species == "NA":
+#READ API TABLES INTO PANDAS DATAFRAMES
+    ncbi_ko = pd.read_table(f"{indir}/ko_ncbi.tsv", dtype=str)
+    ko_pathway = pd.read_table(f"{indir}/link_ko_pathway.tsv", dtype=str)
+    pathway = pd.read_table(f"{indir}/list_pathway.tsv", dtype=str)
+#ADD HEADERS TO DATAFRAME COLUMNS
+    ncbi_ko.columns = ['KEGG_KO', 'Input_protein_ID']
+    ko_pathway.columns = ['KEGG_ref_pathway', 'KEGG_KO']
+    pathway.columns = ['KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+#MERGE DATAFRAMES INTO ONE FOR REFERENCE PATHWAYS
+    ncbi_ko_pathway = pd.merge(ncbi_ko, ko_pathway, on='KEGG_KO', how='inner')
+    ncbi_ko_pathway_pathname = pd.merge(ncbi_ko_pathway, pathway, on='KEGG_ref_pathway', how='left')
+    ncbi_ko_pathway_pathname.insert(0, 'KEGG_genes_ID','NA',allow_duplicates=True)
+    ncbi_ko_pathway_pathname = ncbi_ko_pathway_pathname[['KEGG_genes_ID', 'Input_protein_ID','KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']]
+    ncbi_ko_pathway_pathname.to_csv(f"{outdir}/NA_KEGG_ref.tsv", sep='\t', index=False)
+    if flybase == "FB":
+        #READ INTO DATAFRAMES
+        fbgn_CG = pd.read_table(f"{indir}/Fbgn_CG.tsv", dtype=str)
+        fbgn_path = pd.read_table(f"{indir}/Fbgn_groupid.tsv", dtype=str)
+        fbgn_dia = pd.read_table(f"{indir}/FB_diamond.tsv", dtype=str)
+        fbgn_fbpp = pd.read_table(f"{indir}/Fbgn_fbpp.tsv", dtype=str)
+        #ADD HEADERS
+        fbgn_path.columns = ['Flybase_pathway_ID', 'Flybase_pathway_name', 'Flybase_gene']
+        fbgn_CG.columns = ['Flybase_gene', 'KEGG_genes_ID']
+        fbgn_dia.columns = ['Input_protein_ID', 'Flybase_protein_ID']
+        fbgn_fbpp.columns = ['Flybase_gene', 'Flybase_protein_ID']
+        #MERGE AND OUTPUT TO FILE
+        fbgn_fbpp_dia = pd.merge(fbgn_fbpp, fbgn_dia, on='Flybase_protein_ID', how='inner')
+        fbgn_fbpp_dia_path = pd.merge(fbgn_fbpp_dia, fbgn_path, on='Flybase_gene', how='inner')
+        fbgn_fbpp_dia_path_CG = pd.merge(fbgn_fbpp_dia_path, fbgn_CG, on='Flybase_gene', how='inner')
+        fbgn_fbpp_dia_path_CG.drop('Flybase_gene', axis=1, inplace=True)
+        fbgn_fbpp_dia_path_CG = fbgn_fbpp_dia_path_CG[["KEGG_genes_ID","Input_protein_ID","Flybase_protein_ID","Flybase_pathway_ID","Flybase_pathway_name"]]
+        fbgn_fbpp_dia_path_CG.to_csv(f"{outdir}/Diamond_flybase.tsv", sep='\t', index=False)
+elif kofam == "yes" and species != "NA":
+#READ API TABLES INTO PANDAS DATAFRAMES
+    ncbi_ko = pd.read_table(f"{indir}/ko_ncbi.tsv", dtype=str)
+    ko_pathway = pd.read_table(f"{indir}/link_ko_pathway.tsv", dtype=str)
+    pathway = pd.read_table(f"{indir}/list_pathway.tsv", dtype=str)
+    spec_ko = pd.read_table(f"{indir}/link_{species}_ko.tsv", dtype=str)
+    spec_pathway = pd.read_table(f"{indir}/link_pathway_{species}.tsv", dtype=str)
+    list_pathway_spec = pd.read_table(f"{indir}/list_pathway_{species}.tsv", dtype=str)
+#ADD HEADERS TO DATAFRAME COLUMNS
+    ncbi_ko.columns = ['KEGG_KO', 'Input_protein_ID']
+    spec_ko.columns = ['KEGG_KO', 'KEGG_genes_ID']
+    ko_pathway.columns = ['KEGG_ref_pathway', 'KEGG_KO']
+    pathway.columns = ['KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+    spec_pathway.columns = ['KEGG_genes_ID', f"KEGG_{species}_pathway"]
+    list_pathway_spec.columns = [f"KEGG_{species}_pathway", f"KEGG_{species}_pathway_name"]
+#MERGE DATAFRAMES INTO ONE FOR REFERENCE PATHWAYS
+    ncbi_ko_pathway = pd.merge(ncbi_ko, ko_pathway, on='KEGG_KO', how='inner')
+    ncbi_ko_pathway_pathname = pd.merge(ncbi_ko_pathway, pathway, on='KEGG_ref_pathway', how='left')
+    ncbi_ko_pathway_pathname = ncbi_ko_pathway_pathname[["Input_protein_ID","KEGG_KO","KEGG_ref_pathway","KEGG_ref_pathway_name"]]
+    ncbi_ko_pathway_pathname.insert(0, 'KEGG_genes_ID','NA',allow_duplicates=True)
+    ncbi_ko_pathway_pathname.to_csv(f"{outdir}/{species}_KEGG_ref.tsv", sep='\t', index=False)
+#MERGE DATAFRAMES INTO ONE FOR { species } PATHWAYS
+    ncbi_spec_ko = pd.merge(ncbi_ko, spec_ko, on='KEGG_KO', how='inner')
+    ncbi_spec_ko_specpath = pd.merge(ncbi_spec_ko, spec_pathway, on='KEGG_genes_ID', how='inner')
+    ncbi_spec_ko_specpath_specpathname = pd.merge(ncbi_spec_ko_specpath, list_pathway_spec, on=f"KEGG_{species}_pathway", how='left')
+    ncbi_spec_ko_specpath_specpathname = ncbi_spec_ko_specpath_specpathname[["KEGG_genes_ID","Input_protein_ID","KEGG_KO",f"KEGG_{species}_pathway",f"KEGG_{species}_pathway_name"]]
+    ncbi_spec_ko_specpath_specpathname.to_csv(f"{outdir}/{species}_KEGG_species.tsv", sep='\t', index=False)
+    #ADD FLYBASE ANNOTATIONS
+    if flybase == "FB" and species == "dme":
+        #READ INTO DATAFRAMES
+        fbgn_CG = pd.read_table(f"{indir}/Fbgn_CG.tsv", dtype=str)
+        fbgn_path = pd.read_table(f"{indir}/Fbgn_groupid.tsv", dtype=str)
+        #ADD HEADERS
+        fbgn_CG.columns = ['Flybase_gene', 'KEGG_genes_ID']
+        fbgn_path.columns = ['Flybase_pathway_ID', 'Flybase_pathway_name', 'Flybase_gene']
+        #MERGE AND OUTPUT TO FILE
+        fbgn_CG_path = pd.merge(fbgn_CG, fbgn_path, on='Flybase_gene', how='inner')
+        ncbi_spec_ko['KEGG_genes_ID'] = ncbi_spec_ko['KEGG_genes_ID'].str.replace('Dmel_', '')
+        fbgn_CG_path_ncbi_spec_ko = pd.merge(ncbi_spec_ko, fbgn_CG_path, on='KEGG_genes_ID', how='inner')
+        fbgn_CG_path_ncbi_spec_ko.drop('Flybase_gene', axis=1, inplace=True)
+        fbgn_CG_path_ncbi_spec_ko = fbgn_CG_path_ncbi_spec_ko[["KEGG_genes_ID","Input_protein_ID","KEGG_KO","Flybase_pathway_ID","Flybase_pathway_name"]]
+        fbgn_CG_path_ncbi_spec_ko.to_csv(f"{outdir}/{species}_flybase.tsv", sep='\t', index=False)
+    elif flybase =="FB" and species != "dme":
+        #READ INTO DATAFRAMES
+        fbgn_CG = pd.read_table(f"{indir}/Fbgn_CG.tsv", dtype=str)
+        fbgn_path = pd.read_table(f"{indir}/Fbgn_groupid.tsv", dtype=str)
+        fbgn_dia = pd.read_table(f"{indir}/FB_diamond.tsv", dtype=str)
+        fbgn_fbpp = pd.read_table(f"{indir}/Fbgn_fbpp.tsv", dtype=str)
+        #ADD HEADERS
+        fbgn_path.columns = ['Flybase_pathway_ID', 'Flybase_pathway_name', 'Flybase_gene']
+        fbgn_CG.columns = ['Flybase_gene', 'KEGG_genes_ID']
+        fbgn_dia.columns = ['Input_protein_ID', 'Flybase_protein_ID']
+        fbgn_fbpp.columns = ['Flybase_gene', 'Flybase_protein_ID']
+        #MERGE AND OUTPUT TO FILE
+        fbgn_fbpp_dia = pd.merge(fbgn_fbpp, fbgn_dia, on='Flybase_protein_ID', how='inner')
+        fbgn_fbpp_dia_path = pd.merge(fbgn_fbpp_dia, fbgn_path, on='Flybase_gene', how='inner')
+        fbgn_fbpp_dia_path_CG = pd.merge(fbgn_fbpp_dia_path, fbgn_CG, on='Flybase_gene', how='inner')
+        fbgn_fbpp_dia_path_CG.drop('Flybase_gene', axis=1, inplace=True)
+        fbgn_fbpp_dia_path_CG = fbgn_fbpp_dia_path_CG[["KEGG_genes_ID","Input_protein_ID","Flybase_protein_ID","Flybase_pathway_ID","Flybase_pathway_name"]]
+        fbgn_fbpp_dia_path_CG.to_csv(f"{outdir}/Diamond_flybase.tsv", sep='\t', index=False)
+    else:
+        print("You have not requested Flybase annotations.")
+else:
+    print("Not an acceptable combination of arguments.")
---
+++ pipeline/pull_data.sh
@@ -0,0 +1,103 @@
+#! /bin/bash
+
+#$1 is the KEGG species code (use NA or related species if species not in KEGG)
+#$2 whether kofamscan is necessary ('yes' or 'no')
+#$3 output directory
+#$4 ncbi status of input FASTA accessions ('ncbi' or 'non-ncbi')
+#$5 FB for flybase annotations, NA for none
+
+kegg=$(grep $1 $3/kegg_org_codes.txt)
+
+if [ ! -z "${kegg}" ] && [ $4 == "ncbi" ];
+then
+	#CAN'T PULL THIS FILE FOR SPECIES THAT AREN'T IN KEGG
+	wget https://rest.kegg.jp/conv/ncbi-proteinid/"$1" -O $3/conv_ncbi-proteinid_"$1".tsv
+	sed -i 's/ncbi-proteinid\://g' $3/conv_ncbi-proteinid_"$1".tsv
+	sed -i "s/$1\://g" $3/conv_ncbi-proteinid_"$1".tsv
+fi
+
+if [ $2 == "yes" ];
+then
+	#THIS PULLS THE DATABASE FILES FOR KOFAMSCAN
+	if [ ! -f /data/ko_list ] && [ ! -f /data/ko_list.gz ];
+	then
+		echo "Getting Kofam ko_list."
+		wget https://www.genome.jp/ftp/db/kofam/ko_list.gz -O /data/ko_list.gz
+		gunzip /data/ko_list.gz
+	elif [ -f /data/ko_list.gz ];
+	then
+		echo "Decompressing ko_list."
+		gunzip /data/ko_list.gz
+	else
+		echo "ko_list is already present."
+	fi
+
+
+	if [ ! -d /data/profiles ] && [ ! -f /data/profiles.tar.gz ];
+	then
+		echo "Getting Kofam profiles."
+		wget https://www.genome.jp/ftp/db/kofam/profiles.tar.gz -O /data/profiles.tar.gz
+		tar -xzf /data/profiles.tar.gz -C /data/
+		rm /data/profiles.tar.gz
+	elif [ -f /data/profiles.tar.gz ] && [ ! -d /data/profiles ];
+	then
+		echo "profiles.tar.gz present; decompressing."
+		tar -xzf /data/profiles.tar.gz -C /data/
+		rm /data/profiles.tar.gz
+	else
+		echo "profiles are already present."
+	fi
+fi
+
+
+
+#THESE HAVE TO BE PULLED FOR ALL SPECIES
+wget https://rest.kegg.jp/link/ko/pathway -O $3/link_ko_pathway.tsv
+sed -i 's/ko\://g' $3/link_ko_pathway.tsv
+sed -i 's/path\://g' $3/link_ko_pathway.tsv
+grep -v ko $3/link_ko_pathway.tsv > $3/tmp.txt
+mv $3/tmp.txt $3/link_ko_pathway.tsv
+
+wget https://rest.kegg.jp/list/pathway -O $3/list_pathway.tsv
+
+if [ "$1" != "NA" ];
+then
+	#THESE CAN BE PULLED FOR A RELATED KEGG SPECIES IF DESIRED
+	wget https://rest.kegg.jp/link/pathway/"$1" -O  $3/link_pathway_"$1".tsv
+	sed -i "s/$1\://g" $3/link_pathway_"$1".tsv
+	sed -i 's/path\://g' $3/link_pathway_"$1".tsv
+
+	wget https://rest.kegg.jp/list/pathway/"$1" -O $3/list_pathway_"$1".tsv
+
+	wget https://rest.kegg.jp/link/"$1"/ko -O $3/link_"$1"_ko.tsv
+	sed -i 's/ko\://g' $3/link_"$1"_ko.tsv
+	sed -i "s/$1\://g" $3/link_"$1"_ko.tsv
+fi
+
+if [ "$5" == "FB" ];
+then
+	echo "Pulling Flybase data now."
+	#THIS PULLS THE FLYBASE ANNOTATIONS
+	wget -r -nd -np -A "signaling_pathway_group_data*.gz" -P $3/ 'https://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	wget -r -nd -np -A "metabolic_pathway_group_data*.gz" -P $3/ 'https://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	gunzip -f $3/signaling_pathway_group_data_*.gz
+	gunzip -f $3/metabolic_pathway_group_data_*.gz
+
+	grep -h -v ^\# $3/signaling_pathway_group_data_* > $3/pathway_group_data_latest.tsv
+	grep -h -v ^\# $3/metabolic_pathway_group_data_* >> $3/pathway_group_data_latest.tsv
+	cut -f 1,3,6 $3/pathway_group_data_latest.tsv > $3/Fbgn_groupid.tsv
+
+	wget -r -nd -np -A "fbgn_annotation_ID_fb*.gz" -P $3/ 'https://ftp.flybase.org/releases/current/precomputed_files/genes/'
+	gunzip -f $3/fbgn_annotation_ID_fb*
+	grep -h -v ^\# $3/fbgn_annotation_ID_fb* | cut -f 3,5 > $3/Fbgn_CG.tsv
+	sed -i 's/Dmel_//g' $3/Fbgn_CG.tsv
+
+	#PULL FB PROTEIN FASTA
+	wget -r -nd -np -A "dmel-all-translation*.gz" -P $3/ 'https://ftp.flybase.net/genomes/Drosophila_melanogaster/current/fasta/'
+        gunzip -f $3/dmel-all-translation-*
+
+	#PULL FBGN TO FBPP FILES
+	wget -r -nd -np -A "fbgn_fbtr_fbpp_fb*.gz" -P $3/ 'https://ftp.flybase.net/releases/current/precomputed_files/genes/'
+	gunzip -f $3/fbgn_fbtr_fbpp_fb_*.gz
+	grep -v ^\# $3/fbgn_fbtr_fbpp_fb_* | cut -f 1,3 > $3/Fbgn_fbpp.tsv
+fi
