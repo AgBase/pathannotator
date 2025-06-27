b3771c21e9146a4bf3c6ed8a4ebc994cc4fc8ca0
Amanda Cooksey <amandamcooksey@gmail.com>
2025-06-27 14:51:34 -0700

    cleaning

--- pipeline/pull_data.sh
+++ pipeline/pull_data.sh
@@ -87,15 +87,36 @@ then
 	grep -h -v ^\# $3/metabolic_pathway_group_data_fb_2025_02.tsv >> $3/pathway_group_data_latest.tsv
 	cut -f 1,3,6 $3/pathway_group_data_latest.tsv > $3/Fbgn_groupid.tsv
 
-	wget https://s3ftp.flybase.org/releases/FB2025_02/precomputed_files/genes/fbgn_annotation_ID_fb_2025_02.tsv.gz -P $3
+	#THIS SHOULD PULL THE CURRENT FILE WITHOUT HAVING TO SPEC A VERSION. IT TIMES OUT--AWAITING FB REPLY
+#	wget ftp://ftp.flybase.net/releases/current/precomputed_files/genes/signaling_pathway_group_data*.gz -P $3/
+#	wget ftp://ftp.flybase.net/releases/current/precomputed_files/genes/metabolic_pathway_group_data*.gz -P $3/
+#	gunzip -f $3/signaling_pathway_group_data_*.gz
+#	gunzip -f $3/metabolic_pathway_group_data_*.gz
 
+#	grep -h -v ^\# $3/signaling_pathway_group_data_* > $3/pathway_group_data_latest.tsv
+#	grep -h -v ^\# $3/metabolic_pathway_group_data_* >> $3/pathway_group_data_latest.tsv
+#	cut -f 1,3,6 $3/pathway_group_data_latest.tsv > $3/Fbgn_groupid.tsv
+
+
+	wget https://s3ftp.flybase.org/releases/FB2025_02/precomputed_files/genes/fbgn_annotation_ID_fb_2025_02.tsv.gz -P $3
 	gunzip -f $3/fbgn_annotation_ID_fb_2025_02.tsv.gz
 	grep -h -v ^\# $3/fbgn_annotation_ID_fb_2025_02.tsv | cut -f 3,5 > $3/Fbgn_CG.tsv
 	sed -i 's/Dmel_//g' $3/Fbgn_CG.tsv
 
+	#THIS SHOULD PULL THE CURRENT FILE WITHOUT HAVING TO SPEC A VERSION. IT TIMES OUT--AWAITING FB REPLY
+#	wget ftp://ftp.flybase.org/releases/current/precomputed_files/genes/fbgn_annotation_ID_fb*.gz -P $3/
+#	gunzip -f $3/fbgn_annotation_ID_fb*.gz
+#	grep -h -v ^\# $3/fbgn_annotation_ID_fb*.gz | cut -f 3,5 > $3/Fbgn_CG.tsv
+#	sed -i 's/Dmel_//g' $3/Fbgn_CG.tsv
+
+
 	#PULL FBGN TO FBPP FILES
 	wget https://s3ftp.flybase.org/releases/FB2025_02/precomputed_files/genes/fbgn_fbtr_fbpp_fb_2025_02.tsv.gz -P $3/
-
 	gunzip -f $3/fbgn_fbtr_fbpp_fb_2025_02.tsv.gz
 	grep -v ^\# $3/fbgn_fbtr_fbpp_fb_2025_02.tsv | cut -f 1,3 > $3/Fbgn_fbpp.tsv
+
+#	wget ftp://ftp.flybase.net/releases/current/precomputed_files/genes/fbgn_fbtr_fbpp_fb*.gz -P $3/
+#	gunzip -f $3/fbgn_fbtr_fbpp_fb_*.gz
+#	grep -v ^\# $3/fbgn_fbtr_fbpp_fb_* | cut -f 1,3 > $3/Fbgn_fbpp.tsv
+
 fi
