dc1191e366bca367994778bd0f155c55fd71f127
Amanda Cooksey <amandamcooksey@gmail.com>
2025-06-27 14:54:39 -0700

    adding comment

--- pipeline/pull_data.sh
+++ pipeline/pull_data.sh
@@ -115,6 +115,7 @@ then
 	gunzip -f $3/fbgn_fbtr_fbpp_fb_2025_02.tsv.gz
 	grep -v ^\# $3/fbgn_fbtr_fbpp_fb_2025_02.tsv | cut -f 1,3 > $3/Fbgn_fbpp.tsv
 
+	#THIS SHOULD PULL THE CURRENT FILE WITHOUT HAVING TO SPEC A VERSION. IT TIMES OUT--AWAITING FB REPLY
 #	wget ftp://ftp.flybase.net/releases/current/precomputed_files/genes/fbgn_fbtr_fbpp_fb*.gz -P $3/
 #	gunzip -f $3/fbgn_fbtr_fbpp_fb_*.gz
 #	grep -v ^\# $3/fbgn_fbtr_fbpp_fb_* | cut -f 1,3 > $3/Fbgn_fbpp.tsv
