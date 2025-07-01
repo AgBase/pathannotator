d91bf7a844943067b07f46d5a340de04e9793f5a
Amanda Cooksey <amandamcooksey@gmail.com>
2025-07-01 09:22:52 -0700

    make out dir recursively if necessary

--- pipeline/pathannotator.sh
+++ pipeline/pathannotator.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #CHECK FOR OUTDIR. IF IT DOESN'T EXIST CREATE IT
-if [ ! -d "$3" ]; then mkdir "$3"; fi
+if [ ! -d "$3" ]; then mkdir -p "$3"; fi
 if [ -f "$3"/link_ko_pathway.tsv ]; then rm "$3"/link_ko_pathway.tsv; fi
 if [ -f "$3"/list_pathway.tsv ]; then rm "$3"/list_pathway.tsv; fi
 if [ -f "$3"/conv_ncbi-proteinid_"$1".tsv ]; then rm "$3"/conv_ncbi-proteinid_"$1".tsv; fi
