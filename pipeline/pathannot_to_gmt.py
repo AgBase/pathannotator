3ca623908c140e279c85078aec7ed8da37ac8c06
Amanda Cooksey <amandamcooksey@gmail.com>
2025-09-03 16:16:19 -0700

    updated gmt script to account for optional outputs that may be missing

--- pipeline/pathannot_to_gmt.py
+++ pipeline/pathannot_to_gmt.py
@@ -20,75 +20,150 @@ outbase = args.outbase #basename for output file provided to pathannotator.sh
 
 pd.set_option('display.max_columns', None)
 
-
-#FIND THE OUTPUT FILES TO COMBINE
-keggref = f"{pathannotator}/*KEGG_ref.tsv"
+keggref = (f"{pathannotator}/*KEGG_ref.tsv")
 keggreffile = glob.glob(keggref)
-keggreffile = str(keggreffile[0])
 
-keggspec = f"{pathannotator}/*KEGG_species.tsv"
+keggspec = (f"{pathannotator}/*KEGG_species.tsv")
 keggspecfile = glob.glob(keggspec)
-keggspecfile = str(keggspecfile[0])
 
-flybasetsv = f"{pathannotator}/*flybase.tsv"
-flybasefile = glob.glob(flybasetsv)
-flybasefile = str(flybasefile[0])
+flybase = (f"{pathannotator}/*flybase.tsv")
+flybasefile = glob.glob(flybase)
 
-reactometsv = f"{pathannotator}/*reactome.tsv"
-reactomefile = glob.glob(reactometsv)
-reactomefile = str(reactomefile[0])
+reactome = (f"{pathannotator}/*reactome.tsv")
+reactomefile = glob.glob(reactome)
 
-#READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
-kr = pd.read_table(f"{keggreffile}", dtype=str)
-kr.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']
-ks = pd.read_table(f"{keggspecfile}", dtype=str)
-ks.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_species_pathway', 'KEGG_species_pathway_name']
-fb = pd.read_table(f"{flybasefile}", dtype=str)
-fb.columns = ['Input_protein_ID', 'KEGG_KO', 'Flybase_pathway_ID', 'Flybase_pathway_name']
-rt = pd.read_table(f"{reactomefile}", dtype=str)
-rt.columns = ['Input_protein_ID', 'UniProt_ID', 'Reactome_pathway_ID', 'Reactome_pathway_name']
+if keggreffile != [] and keggspecfile != [] and flybasefile != [] and reactomefile != []:
+    print ("KR, KS, FB, RT outputs are all present")
+    #FIND OUTPUT FILES BASED ON PATTERN MATCH
+    keggreffile = str(keggreffile[0])
+    keggspecfile = str(keggspecfile[0])
+    flybasefile = str(flybasefile[0])
+    reactomefile = str(reactomefile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    kr = pd.read_table(f"{keggreffile}", dtype=str)
+    kr.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+    ks = pd.read_table(f"{keggspecfile}", dtype=str)
+    ks.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_species_pathway', 'KEGG_species_pathway_name']
+    fb = pd.read_table(f"{flybasefile}", dtype=str)
+    fb.columns = ['Input_protein_ID', 'KEGG_KO', 'Flybase_pathway_ID', 'Flybase_pathway_name']
+    rt = pd.read_table(f"{reactomefile}", dtype=str)
+    rt.columns = ['Input_protein_ID', 'UniProt_ID', 'Reactome_pathway_ID', 'Reactome_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    kr = kr.drop(columns=['KEGG_KO', 'KEGG_ref_pathway_name'])
+    ks = ks.drop(columns=['KEGG_KO', 'KEGG_species_pathway_name'])
+    fb = fb.drop(columns=['KEGG_KO', 'Flybase_pathway_name'])
+    rt = rt.drop(columns=['UniProt_ID', 'Reactome_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    ks.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    fb.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    rt.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    kr.insert(loc=1, column='Description', value='KEGG_reference_pathway')
+    ks.insert(loc=1, column='Description', value='KEGG_species_pathway')
+    fb.insert(loc=1, column='Description', value='FlyBase_pathway')
+    rt.insert(loc=1, column='Description', value='Reactome_pathway')
+    #REMOVE DUPLICATE ROWS
+    kr = kr.drop_duplicates()
+    ks = ks.drop_duplicates()
+    fb = fb.drop_duplicates()
+    rt = rt.drop_duplicates()
+    #BRING ALL DATA TOGETHER IN GMT FORMAT
+    alltogether = pd.concat([kr, ks, fb, rt])
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+elif keggreffile != [] and keggspecfile != [] and flybasefile == [] and reactomefile == []:
+    print ("KR and KS outputs are present")
+    #FIND OUTPUT FILES BASED ON PATTERN MATCH
+    keggreffile = str(keggreffile[0])
+    keggspecfile = str(keggspecfile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    kr = pd.read_table(f"{keggreffile}", dtype=str)
+    kr.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+    ks = pd.read_table(f"{keggspecfile}", dtype=str)
+    ks.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_species_pathway', 'KEGG_species_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    kr = kr.drop(columns=['KEGG_KO', 'KEGG_ref_pathway_name'])
+    ks = ks.drop(columns=['KEGG_KO', 'KEGG_species_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    ks.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    kr.insert(loc=1, column='Description', value='KEGG_reference_pathway')
+    ks.insert(loc=1, column='Description', value='KEGG_species_pathway')
+    #REMOVE DUPLICATE ROWS
+    kr = kr.drop_duplicates()
+    ks = ks.drop_duplicates()
+    #BRING ALL DATA TOGETHER IN GMT FORMAT
+    alltogether = pd.concat([kr, ks])
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+elif keggreffile != [] and keggspecfile == []  and flybasefile != [] and reactomefile != []:
+    print ("KR, FB and RT outputs are present")
+    #FIND OUTPUT FILES BASED ON PATTERN MATCH
+    keggreffile = str(keggreffile[0])
+    flybasefile = str(flybasefile[0])
+    reactomefile = str(reactomefile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    kr = pd.read_table(f"{keggreffile}", dtype=str)
+    kr.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+    fb = pd.read_table(f"{flybasefile}", dtype=str)
+    fb.columns = ['Input_protein_ID', 'KEGG_KO', 'Flybase_pathway_ID', 'Flybase_pathway_name']
+    rt = pd.read_table(f"{reactomefile}", dtype=str)
+    rt.columns = ['Input_protein_ID', 'UniProt_ID', 'Reactome_pathway_ID', 'Reactome_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    kr = kr.drop(columns=['KEGG_KO', 'KEGG_ref_pathway_name'])
+    fb = fb.drop(columns=['KEGG_KO', 'Flybase_pathway_name'])
+    rt = rt.drop(columns=['UniProt_ID', 'Reactome_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    fb.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    rt.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    kr.insert(loc=1, column='Description', value='KEGG_reference_pathway')
+    fb.insert(loc=1, column='Description', value='FlyBase_pathway')
+    rt.insert(loc=1, column='Description', value='Reactome_pathway')
+    #REMOVE DUPLICATE ROWS
+    kr = kr.drop_duplicates()
+    fb = fb.drop_duplicates()
+    rt = rt.drop_duplicates()
+    #BRING ALL DATA TOGETHER IN GMT FORMAT
+    alltogether = pd.concat([kr, fb, rt])
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+elif keggreffile != [] and keggspecfile == [] and flybasefile == [] and reactomefile == []:
+    print ("only KR output is present")
+    #FIND OUTPUT FILES BASED ON PATTERN MATCH
+    keggreffile = str(keggreffile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    kr = pd.read_table(f"{keggreffile}", dtype=str)
+    kr.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    kr = kr.drop(columns=['KEGG_KO', 'KEGG_ref_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    kr.insert(loc=1, column='Description', value='KEGG_reference_pathway')
+    #REMOVE DUPLICATE ROWS
+    kr = kr.drop_duplicates()
+    #BRING ALL DATA TOGETHER IN GMT FORMAT
+    alltogether = kr
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+else:
+    print ("Cannot find the proper combination of output files.")
 
-#DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
-kr = kr.drop(columns=['KEGG_KO', 'KEGG_ref_pathway_name'])
-ks = ks.drop(columns=['KEGG_KO', 'KEGG_species_pathway_name'])
-fb = fb.drop(columns=['KEGG_KO', 'Flybase_pathway_name'])
-rt = rt.drop(columns=['UniProt_ID', 'Reactome_pathway_name'])
-
-#MAKE HEADERS MATCH FOR ALL DFS
-kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
-ks.columns = ['Input_protein_ID', 'Pathway_or_domain']
-fb.columns = ['Input_protein_ID', 'Pathway_or_domain']
-rt.columns = ['Input_protein_ID', 'Pathway_or_domain']
-
-#ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
-kr.insert(loc=1, column='Description', value='KEGG_reference_pathway')
-ks.insert(loc=1, column='Description', value='KEGG_species_pathway')
-fb.insert(loc=1, column='Description', value='FlyBase_pathway')
-rt.insert(loc=1, column='Description', value='Reactome_pathway')
-
-#REMOVE DUPLICATE ROWS
-kr = kr.drop_duplicates()
-ks = ks.drop_duplicates()
-fb = fb.drop_duplicates()
-rt = rt.drop_duplicates()
-
-#JOIN DFS INTO ONE
-alltogether = pd.concat([kr, ks, fb, rt])
-
-#RUN GROUPBY
-alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
-
-#SPLIT LIST INTO SEPARATE COLUMNS
-justlist = alltogether['Input_protein_ID'].apply(pd.Series)
-alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
-
-#PRINT TO TSV WITHOUT HEADER
-alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
-
-#REMOVE TRAILING TABS
-gmtfile=f"{outdir}/{outbase}_all_and_pathways.gmt"
+#REMOVE ANY TRAILING TABS FROM EMPTY DF FIELDS
+gmtfile=f"{outdir}/{outbase}_all_pathways.gmt"
 notab=f"{outdir}/gmt.tmp"
-
 
 with open(gmtfile, 'r') as infile, open(notab, 'w') as outfile:
     for line in infile:
@@ -96,3 +171,116 @@ with open(gmtfile, 'r') as infile, open(notab, 'w') as outfile:
         outfile.write(cleaned_line)
 
 os.rename(notab, gmtfile)
+
+#############################################################################################################
+'''
+#FIND THE OUTPUT FILES TO COMBINE
+keggref = f"{pathannotator}/*KEGG_ref.tsv"
+keggreffile = glob.glob(keggref)
+keggreffile = str(keggreffile[0])
+#READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+kr = pd.read_table(f"{keggreffile}", dtype=str)
+kr.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_ref_pathway', 'KEGG_ref_pathway_name']
+#DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+kr = kr.drop(columns=['KEGG_KO', 'KEGG_ref_pathway_name'])
+#MAKE HEADERS MATCH FOR ALL DFS
+kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
+#ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+kr.insert(loc=1, column='Description', value='KEGG_reference_pathway')
+#REMOVE DUPLICATE ROWS
+kr = kr.drop_duplicates()
+
+if os.path.exists(f"{pathannotator}/*KEGG_species.tsv"):
+#keggspec = f"{pathannotator}/*KEGG_species.tsv"
+    keggspecfile = glob.glob(keggspec)
+    keggspecfile = str(keggspecfile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    ks = pd.read_table(f"{keggspecfile}", dtype=str)
+    ks.columns = ['Input_protein_ID', 'KEGG_KO', 'KEGG_species_pathway', 'KEGG_species_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    ks = ks.drop(columns=['KEGG_KO', 'KEGG_species_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    kr.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    ks.insert(loc=1, column='Description', value='KEGG_species_pathway')
+    #REMOVE DUPLICATE ROWS
+    ks = ks.drop_duplicates()
+
+if os.path.exists(f"{pathannotator}/*flybase.tsv"):
+#flybase = f"{pathannotator}/*flybase.tsv"
+    flybasefile = glob.glob(flybasetsv)
+    flybasefile = str(flybasefile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    fb = pd.read_table(f"{flybasefile}", dtype=str)
+    fb.columns = ['Input_protein_ID', 'KEGG_KO', 'Flybase_pathway_ID', 'Flybase_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    fb = fb.drop(columns=['KEGG_KO', 'Flybase_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    fb.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    fb.insert(loc=1, column='Description', value='FlyBase_pathway')
+    #REMOVE DUPLICATE ROWS
+    fb = fb.drop_duplicates()
+
+if os.path.exists(f"{pathannotator}/*reactome.tsv"):
+#reactome = f"{pathannotator}/*reactome.tsv"
+    reactomefile = glob.glob(reactometsv)
+    reactomefile = str(reactomefile[0])
+    #READ TABLES INTO PANDAS DATAFRAMES AND ADD HEADERS TO IPRS
+    rt = pd.read_table(f"{reactomefile}", dtype=str)
+    rt.columns = ['Input_protein_ID', 'UniProt_ID', 'Reactome_pathway_ID', 'Reactome_pathway_name']
+    #DROP UNWANTED COLUMNS (KEEP PROTEIN ACCESSIONS (1), INTERPRO ANNOTATIONS (12) AND PATHWAY ANNOTATIONS (15)
+    rt = rt.drop(columns=['UniProt_ID', 'Reactome_pathway_name'])
+    #MAKE HEADERS MATCH FOR ALL DFS
+    rt.columns = ['Input_protein_ID', 'Pathway_or_domain']
+    #ADD SECOND COLUMN TO EACH DF WITH 'OPTIONAL DESCRIPTION' FOR GMT FORMAT
+    rt.insert(loc=1, column='Description', value='Reactome_pathway')
+    #REMOVE DUPLICATE ROWS
+    rt = rt.drop_duplicates()
+
+#if 'keggreffile' in locals() and 'keggspecfile' in locals() and 'flybasefile' in locals() and 'reactomefile' in locals():
+if not kr.empty and not ks.empty and not fb.empty and not rt.empty:
+    print ("KR, KS, FB, RT outputs are all present")
+    alltogether = pd.concat([kr, ks, fb, rt])
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+#elif 'keggreffile' in locals() and 'keggspecfile' in locals() and 'flybasefile' not in locals() and 'reactomefile' not in locals():
+elif not kr.empty and not ks.empty and fb.empty and rt.empty:
+    print ("KR and KS outputs are present")
+    alltogether = pd.concat([kr, ks])
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+#elif 'keggreffile' in locals() and 'keggspecfile' not in locals() and 'flybasefile' in locals() and 'reactomefile' in locals():
+elif not kr.empty and ks.empty and not rb.empty and not rt.empty:
+    print ("KR, FB and RT outputs are present")
+    alltogether = pd.concat([kr, fb, rt])
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+#elif 'keggreffile' in locals() and 'keggspecfile' not in locals() and 'flybasefile' not in locals() and 'reactomefile' not in locals():
+elif not kr.empty and ks.empty  and fb.empty and rt.empty:
+    print ("only KR output is present")
+    alltogether = kr
+    alltogether = alltogether.groupby(['Pathway_or_domain', 'Description'])['Input_protein_ID'].agg(list).reset_index()
+    justlist = alltogether['Input_protein_ID'].apply(pd.Series)
+    alltogether = pd.concat([alltogether.drop('Input_protein_ID', axis=1), justlist], axis=1)
+    alltogether.to_csv(f"{outdir}/{outbase}_all_pathways.gmt", sep='\t', header=False, index=False)
+else:
+    print ("Cannot find the proper combination of output files.")
+
+#REMOVE ANY TRAILING TABS FROM EMPTY DF FIELDS
+gmtfile=f"{outdir}/{outbase}_all_pathways.gmt"
+notab=f"{outdir}/gmt.tmp"
+
+with open(gmtfile, 'r') as infile, open(notab, 'w') as outfile:
+    for line in infile:
+        cleaned_line = re.sub(r'\t+\n', '\n', line)
+        outfile.write(cleaned_line)
+
+os.rename(notab, gmtfile)
+'''
